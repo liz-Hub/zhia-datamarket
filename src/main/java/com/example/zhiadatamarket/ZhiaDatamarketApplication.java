f4f826a8253539ec4c669083475c3caec0943144
lizhi <18345339912@163.com>
2023-05-26 11:31:28 +0800

    Initial commit

---
+++ src/main/java/com/example/zhiadatamarket/ZhiaDatamarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.zhiadatamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhiaDatamarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhiaDatamarketApplication.class, args);
+    }
+
+}
